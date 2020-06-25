07019e7eca68456e98de8b1df5c2d3dbf0f0be81
Luan Santos <luan__122@hotmail.com>
2020-06-25 17:02:33 -0300

    Add new method to search with no tracking

--- 2.Domain/ROAPI.Domain.Character/Contracts/Repositories/ICharacterRepository.cs
+++ 2.Domain/ROAPI.Domain.Character/Contracts/Repositories/ICharacterRepository.cs
@@ -3,10 +3,12 @@ using ROAPI.Infrastructure.Data.Data.Interfaces.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ROAPI.Domain.Character.Contracts.Repositories
 {
     public interface ICharacterRepository:IBaseRepository<CharacterEntity>
     {
+        Task<CharacterEntity> GetByIdNoTracking(int id);
     }
 }
--- 2.Domain/ROAPI.Domain.Character/Repositories/CharacterRepository.cs
+++ 2.Domain/ROAPI.Domain.Character/Repositories/CharacterRepository.cs
@@ -5,11 +5,18 @@ using ROAPI.Infrastructure.Data.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace ROAPI.Domain.Character.Repositories
 {
     public class CharacterRepository:BaseRepository<CharacterEntity, MainDbContext>, ICharacterRepository
     {
         public CharacterRepository(MainDbContext context) : base(context) { }
+        public async Task<CharacterEntity> GetByIdNoTracking(int id)
+        {
+            return await DbSet.AsNoTracking().Where(c => c.char_id == id).FirstOrDefaultAsync();
+        }
     }
 }
